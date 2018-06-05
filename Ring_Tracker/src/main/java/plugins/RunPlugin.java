4bbe32ca8c2723c52f89c27c45e1a9a8a1f06060
ryan <ryan@dundon>
2018-06-05 13:38:50 -0400

    Got RunPlugin to processor image pixel by pixel

--- Ring_Tracker/src/main/java/plugins/RunPlugin.java
+++ Ring_Tracker/src/main/java/plugins/RunPlugin.java
@@ -6,28 +6,37 @@ import ij.ImagePlus;
 import ij.plugin.filter.PlugInFilter;
 
 import ij.process.ImageProcessor;
+import segmentation.AverageIntensity;
 import utils.Pixel;
 
 public class RunPlugin implements PlugInFilter{
 	ImageProcessor processor;
 	ImagePlus image;
+	Pixel[] pixelArray;
 	
 	
 	
 	@Override
 	public void run(ImageProcessor imgProcessor) {
-		processor = imgProcessor;
+		processor = imgProcessor.duplicate();
 		
 		// Sizes of the current imageProcessor
 		int imageHeight = processor.getHeight();
-		System.out.println(imageHeight);
+		System.out.println("Image Height: " + imageHeight);
 		int imageWidth = processor.getWidth();	
-		System.out.println(imageWidth);
+		System.out.println("Image Width: " + imageWidth);
 		
-	
+		// Create and fill pixelArray
+		pixelArray = new Pixel[imageHeight * imageWidth];
+		System.out.println("pixelArray size: " + pixelArray.length);
+		
+		// Set values below threshold to 0
+		for (int i=0; i<processor.getPixelCount(); i++) {
+			if (processor.get(i) < 160) processor.set(i, 0);
+		}
 		
 		// Create and show new image
-		image = new ImagePlus("Cell Mask", imgProcessor);
+		image = new ImagePlus("Cell Mask", processor);
 		image.show();
 		
 	}
